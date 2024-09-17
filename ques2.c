1c870bd9a383f555fae5bd79d4f433074d48c2f3
PihooGokhle <pihoogokhle@gmail.com>
2024-09-17 14:54:05 +0530

    add ques2.c file

---
+++ ques2.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main(){
+     
+     float r;
+
+     printf("enter radius ");
+     scanf("%f",&r);
+
+     float circumference = 2 * 3.14 * r;
+
+     printf("circumference of circle is %.2f",circumference);
+
+     return 0;
+}
